ff8b978df02af5d634af41b4425236377a537b39
Maria <mit.maria2019@yandex.ru>
2023-09-05 11:38:29 +0300

    feat: верстка формы резюме

---
+++ task3-2/script.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            profession: "",
+            city: "",
+            photo: "",
+            fullName: "",
+            phone: "",
+            email: "",
+            birthdate: "",
+            education: "",
+            desiredSalary: "",
+            skills: "",
+            aboutMe: "",
+        };
+    },
+
+    mounted() {},
+
+    methods: {},
+});
+
+app.mount("#app");
