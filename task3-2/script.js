95a7c02e7eca0d1b47f1a7a14a2a4a265533749a
Maria <mit.maria2019@yandex.ru>
2023-09-07 01:26:27 +0300

    feat: добавлен год окончания и отрисовка по условию

--- task3-2/script.js
+++ task3-2/script.js
@@ -12,12 +12,49 @@ const app = Vue.createApp({
             desiredSalary: "",
             skills: "",
             aboutMe: "",
+            educationList: {
+                middle: 'Среднее',
+                middleSpec: 'Среднее специальное',
+                semiHigh: 'Неоконченное высшее',
+                universe: 'Высшее'
+            },
+            eduDetail: {
+                institution: {
+                    title: "Учебное заведение",
+                    value: ""
+                },
+                faculty: {
+                    title: "Факультет",
+                    value: ""
+                },
+                specialization: {
+                    title: "Специальность",
+                    value: ""
+                },
+                endYear: {
+                    title: "Год окончания",
+                    value: "",
+                    type: "number"
+                },
+            },
+            showResumeFlag: false
+            
         };
     },
 
     mounted() {},
 
-    methods: {},
+    methods: {
+        showResume(event) {
+            this.showResumeFlag = true
+        }
+    },
+
+    computed: {
+        getProfession() {
+            return this.profession + ' бла-бла'
+        }
+    }
 });
 
 app.mount("#app");
