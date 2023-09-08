e9afa51def4c501e3d9ff0730789fbb0ea488e1a
Maria <mit.maria2019@yandex.ru>
2023-09-08 19:24:51 +0300

    feat: валидация телефона, вывод по нажатию

--- task3-2/script.js
+++ task3-2/script.js
@@ -1,60 +1,134 @@
 const app = Vue.createApp({
     data() {
         return {
-            profession: "",
-            city: "",
-            photo: "",
-            fullName: "",
-            phone: "",
-            email: "",
-            birthdate: "",
-            education: "",
-            desiredSalary: "",
-            skills: "",
-            aboutMe: "",
-            educationList: {
-                middle: 'Среднее',
-                middleSpec: 'Среднее специальное',
-                semiHigh: 'Неоконченное высшее',
-                universe: 'Высшее'
+            enterData: {
+                profession: "",
+                city: "",
+                photo: "",
+                fullName: "",
+                phone: "",
+                email: "",
+                birthdate: "",
+                education: "",
+                desiredSalary: "",
+                skills: "",
+                aboutMe: "",
+                institution: "",
+                faculty: "",
+                specialization: "",
+                endYear: "",
             },
-            eduDetail: {
+            resumeData: {
+                profession: "",
+                city: "",
+                photo: "",
+                fullName: "",
+                phone: "",
+                email: "",
+                birthdate: "",
+                education: "",
+                desiredSalary: "",
+                skills: "",
+                aboutMe: "",
+                institution: "",
+                faculty: "",
+                specialization: "",
+                endYear: "",
+            },
+            educationList: {
+                middle: "Среднее",
+                middleSpec: "Среднее специальное",
+                semiHigh: "Неоконченное высшее",
+                universe: "Высшее",
+            },
+            fields: {
+                profession: {
+                    title: "Профессия",
+                },
+                city: {
+                    title: "Город",
+                },
+                photo: {
+                    title: "Фото",
+                    type: 'image'
+                },
+                fullName: {
+                    title: "ФИО",
+                },
+                phone: {
+                    title: "Телефон",
+                },
+                email: {
+                    title: "Email",
+                },
+                birthdate: {
+                    title: "Дата рождения",
+                },
+                education: {
+                    title: "Образование",
+                },
+                desiredSalary: {
+                    title: "Желаемая зарплата",
+                },
+                skills: {
+                    title: "Ключевые навыки",
+                },
+                aboutMe: {
+                    title: "О себе",
+                },
+            },
+
+            eduDetailFields: {
                 institution: {
                     title: "Учебное заведение",
-                    value: ""
                 },
                 faculty: {
                     title: "Факультет",
-                    value: ""
                 },
                 specialization: {
                     title: "Специальность",
-                    value: ""
                 },
                 endYear: {
                     title: "Год окончания",
-                    value: "",
-                    type: "number"
+                    type: "number",
                 },
             },
-            showResumeFlag: false
-            
+
+            showResumeFlag: false,
         };
     },
 
     mounted() {},
 
     methods: {
-        showResume(event) {
-            this.showResumeFlag = true
-        }
+        showResume() {
+            this.showResumeFlag = true;
+
+            for (let key in this.enterData) {
+                this.resumeData[key] = this.enterData[key];
+
+                if (key == "education") {
+                    this.resumeData[key] =
+                        this.educationList[this.enterData[key]];
+                }
+            }
+        },
     },
 
     computed: {
         getProfession() {
-            return this.profession + ' бла-бла'
-        }
-    }
+            return this.profession + " бла-бла";
+        },
+        validatePhone() {
+            const phoneNumberPattern = /^[0-9]{6,10}$/;
+
+            if (phoneNumberPattern.test(this.enterData.phone)) {
+                return "";
+            } else {
+                return "Номер телефона должен состоять из 6-10 цифр.";
+            }
+        },
+    },
 });
 
 app.mount("#app");
