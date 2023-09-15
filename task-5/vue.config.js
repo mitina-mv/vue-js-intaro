f5b45754f821ce5ec1fa1dc799d47f984ee259dd
Maria <mit.maria2019@yandex.ru>
2023-09-15 10:41:34 +0300

    core: настройка конфигруации вебпака через конфиг вью

--- task-5/vue.config.js
+++ task-5/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    output: {
+      filename: 'js/masha_[name].js',
+    }
+  }
 })
