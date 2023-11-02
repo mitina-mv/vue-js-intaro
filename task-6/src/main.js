33deb37abcb85f1a0cff5702960eef049bac21bd
Maria <mit.maria2019@yandex.ru>
2023-11-02 16:22:31 +0300

    feat: подключила prime vue для красивых элементиков

--- task-6/src/main.js
+++ task-6/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/soho-light/theme.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(PrimeVue)
+.mount('#app')
+
